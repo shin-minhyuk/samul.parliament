992566719a474e6467e18b824a66216d220f4dca
shin-minhyuk <afs_style@naver.com>
2025-03-09 00:11:13 +0900

    [feat] 히어로배너 스타일 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,15 @@ export default function Home() {
       <Header />
       <main className="pt-24">
         {/* Main content will go here */}
-        <section className="flex h-[600px] items-center justify-center">
-          <div className="group hover:animate-wiggle flex flex-col gap-4 text-6xl font-bold">
-            <p>2025 Environment</p>
-            <p className="text-p-electric group-hover:text-p-lime transform text-8xl transition-all duration-300">
-              사물과 인간
-            </p>
-            <p>함께 논의하는 새로운 미래</p>
+        <section className="flex h-[600px] items-center justify-center overflow-hidden">
+          <div className="animate-slide-up">
+            <div className="group hover:animate-wiggle flex flex-col gap-4 text-8xl font-bold">
+              <p>2025 Environment</p>
+              <p className="text-p-electric group-hover:text-p-lime transform text-9xl transition-all duration-300">
+                사물과 인간
+              </p>
+              <p>함께 논의하는 새로운 미래</p>
+            </div>
           </div>
         </section>
       </main>
