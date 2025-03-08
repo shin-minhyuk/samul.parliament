f93795d4d530a69e04559a6d31e8c618bf544d52
shin-minhyuk <afs_style@naver.com>
2025-03-09 00:51:38 +0900

    [feat] 서브 배너 애니메이션 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,22 +21,25 @@ export default function Home() {
         {/* 서브 배너 */}
         <section className="h-[440px] border border-red-500">
           {/* 서브 배너 */}
-          <div className="bg-nature-forest relative flex h-[360px] items-center justify-center text-white">
-            <div className="flex flex-col gap-4 text-center text-2xl">
-              <p className="font-semibold">함께 고민하는 시간</p>
-              <p className="text-5xl font-bold">
-                비인간 존재들의 <span className="text-nature-spring">권리</span>
-                와 <span className="text-nature-spring">발언권</span>을 고민하다
-              </p>
-              <p className="text-xl font-bold opacity-50">
-                2025 사물의 의회 주제 : 미래의 공존을 위한 새로운 논의
-              </p>
-            </div>
+          <div>
+            <div className="bg-nature-forest border-nature-spring relative flex h-[360px] items-center justify-center border-y-[20px] text-white">
+              <div className="animate-slide-up flex flex-col gap-4 text-center text-2xl">
+                <p className="font-semibold">함께 고민하는 시간</p>
+                <p className="text-5xl font-bold">
+                  비인간 존재들의{" "}
+                  <span className="text-nature-spring">권리</span>와
+                  <span className="text-nature-spring">발언권</span>을 고민하다
+                </p>
+                <p className="text-xl font-bold opacity-50">
+                  2025 사물의 의회 주제 : 미래의 공존을 위한 새로운 논의
+                </p>
+              </div>
 
-            {/* 참가버튼 */}
-            <button className="bg-nature-leaf hover:bg-nature-spring hover:animate-wiggle absolute right-0 bottom-[-50px] left-0 mx-auto h-[100px] w-[100px] rounded-full px-4 py-2 text-sm font-semibold text-white transition-all">
-              신청하기
-            </button>
+              {/* 참가버튼 */}
+              <button className="animate-rotate-in bg-nature-leaf hover:bg-nature-spring hover:animate-wiggle text-md absolute right-0 bottom-[-50px] left-0 mx-auto h-[100px] w-[100px] rounded-full px-4 py-2 font-bold text-white transition-all">
+                <p>신청하기</p>
+              </button>
+            </div>
           </div>
         </section>
       </main>
