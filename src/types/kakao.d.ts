2c85d2d9351a7a75fedfbd94ff8cdebb48ce29c9
shin-minhyuk <afs_style@naver.com>
2025-03-15 17:22:15 +0900

    [feat] 카카오 맵스 API window 객체 타입 추가

--- src/types/kakao.d.ts
+++ src/types/kakao.d.ts
@@ -1,12 +1,41 @@
+interface KakaoLatLng {
+  getLat(): number;
+  getLng(): number;
+}
+
+interface KakaoMap {
+  setCenter(position: KakaoLatLng): void;
+  getCenter(): KakaoLatLng;
+  setLevel(level: number): void;
+  getLevel(): number;
+}
+
+interface KakaoMarker {
+  setMap(map: KakaoMap | null): void;
+  getMap(): KakaoMap | null;
+}
+
+interface KakaoInfoWindow {
+  open(map: KakaoMap, marker: KakaoMarker): void;
+  close(): void;
+}
+
 interface KakaoMapAPI {
   maps: {
     load: (callback: () => void) => void;
-    LatLng: new (lat: number, lng: number) => any;
-    Map: new (container: HTMLElement, options: any) => any;
-    Marker: new (options: any) => any;
-    InfoWindow: new (options: any) => any;
+    LatLng: new (lat: number, lng: number) => KakaoLatLng;
+    Map: new (
+      container: HTMLElement,
+      options: { center: KakaoLatLng; level: number },
+    ) => KakaoMap;
+    Marker: new (options: { position: KakaoLatLng }) => KakaoMarker;
+    InfoWindow: new (options: { content: string }) => KakaoInfoWindow;
     event: {
-      addListener: (target: any, type: string, handler: () => void) => void;
+      addListener: (
+        target: KakaoMarker | KakaoMap,
+        type: string,
+        handler: () => void,
+      ) => void;
     };
   };
 }
