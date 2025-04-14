a8f286a89234171c784cecf83d7c93f680115b42
shin-minhyuk <afs_style@naver.com>
2025-04-14 23:09:07 +0900

    [feat] 이미지 최적화 포맷 수정, 기존 jpg, png -> avif, webp 포맷으로 사용, 이미지 캐시 타임 60초, CSS 파일 최적화

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     domains: ["firebasestorage.googleapis.com"],
+    formats: ["image/avif", "image/webp"],
+    minimumCacheTTL: 60,
+  },
+  experimental: {
+    optimizeCss: true,
   },
 };
 
